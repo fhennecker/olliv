f63dbd9482b629f888f4bab2eec5146a89597e96
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-04-25 18:38:04 +0200

    [add] bikes import, list of bikes available at a station

--- src/importData.py
+++ src/importData.py
@@ -3,6 +3,7 @@ import sqlite3
 DATABASE_PATH = '../data/data.db'
 
 STATIONS_PATH = '../data/stations.csv'
+BIKES_PATH = '../data/villos.csv'
 
 connexion = sqlite3.connect(DATABASE_PATH)
 c = connexion.cursor()
@@ -11,8 +12,30 @@ c = connexion.cursor()
 with open(STATIONS_PATH, 'r') as f:
     for line in f.readlines()[1:] : # skipping the first line (header)
         values = line.strip().split(";")
-        print values
         c.execute("INSERT INTO Stations VALUES (?, ?, ?, ?, ?, ?)", values)
+
+# importing bikes
+with open(BIKES_PATH, 'r') as f:
+    # selecting capacity as well to make sure no station exceeds its capacity
+    capacities = map(list, c.execute("SELECT id, capacity FROM Stations").fetchall())
+    i = 0 # index of the capacities list
+    station = 0 # the station we'll add a bike to
+
+    for line in f.readlines()[1:] :
+        number, commissionDate, model, state = line.strip().split(";")
+        commissionDate = commissionDate.replace("T", " ")
+        c.execute("INSERT INTO Bikes VALUES (?, ?, ?, ?, ?)", (number, commissionDate, model, state, station))
+
+        # decreasing capacity of the station
+        capacities[i][1] = capacities[i][1] - 1
+        # chooses the next station in the list which still has capacity
+        i = (i+1) % len(capacities)
+        station = capacities[i][0]
+        while capacities[i][1] == 0 :
+            i = (i+1) % len(capacities)
+            station = capacities[i][0]
+
+
 
 connexion.commit()
 connexion.close()
--- src/main.py
+++ src/main.py
@@ -1,9 +1,47 @@
-from flask import Flask
+from flask import Flask, g, render_template, abort
+import sqlite3
 app = Flask(__name__)
 
+################################################################################
+# Database setup and teardown
+DATABASE = '../data/data.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+################################################################################
+# Routes
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!<br/><a href="/stations">See the list of stations &gt;</a>'
+
+@app.route('/stations')
+def display_stations():
+    c = get_db().cursor()
+    stations = c.execute("SELECT id, name FROM Stations").fetchall()
+    return render_template("stations.html", stations=stations)
+
+@app.route('/station/<station_id>')
+def display_station(station_id):
+    c = get_db().cursor()
+    print station_id
+    station = c.execute("SELECT * FROM Stations WHERE id == (?)", (station_id,)).fetchone()
+    bikes = c.execute("SELECT id FROM Bikes WHERE station == (?)", (station_id,)).fetchall()
+    if station is None:
+        abort(404) 
+    return render_template("station.html", station=station, bikes=bikes)
+
+
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
