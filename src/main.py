c65a0d403b04cc57a98c41b3514eff82e3a9544e
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-04-25 16:04:20 +0200

    [add] /stations page to test querying the DB from flask and the template rendering engine

--- src/main.py
+++ src/main.py
@@ -1,9 +1,37 @@
-from flask import Flask
+from flask import Flask, g, render_template
+import sqlite3
 app = Flask(__name__)
 
+################################################################################
+# Database setup and teardown
+DATABASE = '../data/data.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+################################################################################
+# Routes
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!<br/><a href="/stations">See the list of stations &gt;</a>'
+
+@app.route('/stations')
+def display_stations():
+    c = get_db().cursor()
+    stations = c.execute("SELECT name FROM Stations").fetchall()
+    return render_template("stations.html", stations=stations)
+
+
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
