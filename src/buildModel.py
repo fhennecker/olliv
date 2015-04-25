9519a43c0f9dc707711fb7da608bcd2e439bf521
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-04-25 13:00:29 +0200

    [add] ArgParse for easy and automated database recreation

--- src/buildModel.py
+++ src/buildModel.py
@@ -1,11 +1,25 @@
 import sqlite3
+import os
+import argparse
 
-connexion = sqlite3.connect('../data/data.db')
+DATABASE_PATH = "../data/data.db"
+
+# allowing command-line options
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--delete", help="delete the currently existing database", 
+                                      action="store_true")
+args = parser.parse_args()
+
+# deleting the existing database if needed
+if args.delete:
+    os.remove(DATABASE_PATH)
+
+
+# sqlite3.connect will create the database if it doesn't exist yet
+connexion = sqlite3.connect(DATABASE_PATH)
 c = connexion.cursor()
 
-c.execute('''DROP TABLE stations''')
-
-c.execute('''CREATE TABLE stations(
+c.execute('''CREATE TABLE Stations(
                 id          INT             NOT NULL,
                 name        VARCHAR(64),
                 gpsx        DECIMAL(9,6),
