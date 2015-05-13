0d15b7ecc6dbbf49e2ca7807d6ccbe835c091342
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-05-13 16:08:26 +0200

    [enh] moved the DDL script in its own file

--- src/buildModel.py
+++ src/buildModel.py
@@ -18,68 +18,8 @@ if args.delete:
 connexion = sqlite3.connect(DATABASE_PATH)
 c = connexion.cursor()
 
-c.execute('''CREATE TABLE Stations(
-                id          INT UNSIGNED        NOT NULL,
-                name        VARCHAR(64),
-                payTerminal BOOLEAN,
-                capacity    SMALLINT UNSIGNED,
-                gpsx        DECIMAL(9,6),
-                gpsy        DECIMAL(9,6),
-
-                PRIMARY KEY (id)
-                )''')
-
-c.execute('''CREATE TABLE Bikes(
-                id              MEDIUMINT UNSIGNED      NOT NULL,
-                commissionDate  DATE,
-                model           VARCHAR(64),
-                state           TEXT,
-                station         INT UNSIGNED,
-
-                PRIMARY KEY (id),
-                FOREIGN KEY (station) REFERENCES Stations(id)
-                )''')
-
-c.execute('''CREATE TABLE Users(
-                id              INT UNSIGNED        NOT NULL,
-                password        TINYTEXT,
-                expiryDate      DATETIME,
-                cardNumber      BIGINT(16),
-
-                PRIMARY KEY (id)
-                )''')
-
-c.execute('''CREATE TABLE Subscribers(
-                id                  INT UNSIGNED        NOT NULL,
-                rfid                INT UNSIGNED,
-                lastName            TINYTEXT,
-                firstName           TINYTEXT,
-                addressTown         TINYTEXT,
-                addressZIP          MEDIUMINT,
-                addressStreet       TINYTEXT,
-                addressNumber       VARCHAR(10),
-                registrationDate    DATETIME,
-                phone               VARCHAR(20),
-
-                PRIMARY KEY (id),
-                FOREIGN KEY (id) REFERENCES Users(id)
-                )''')
-
-c.execute('''CREATE TABLE Trips(
-                user            INT UNSIGNED      NOT NULL,
-                startDate       TEXT,
-                endDate         TEXT,
-                startStation    INT UNSIGNED,
-                endStation      INT UNSIGNED,
-                bike            INT UNSIGNED,
-                paid            BOOLEAN,
-
-                PRIMARY KEY (user, startDate),
-                FOREIGN KEY (user) REFERENCES Users(id),
-                FOREIGN KEY (startStation) REFERENCES Stations(id),
-                FOREIGN KEY (endStation) REFERENCES Stations(id),
-                FOREIGN KEY (bike) REFERENCES Bikes(id)
-                )''')
+for statement in open("initialization.sql", 'r').read().split(";"):
+    c.execute(statement)
 
 connexion.commit()
 connexion.close()
---
+++ src/initialization.sql
@@ -0,0 +1,62 @@
+CREATE TABLE Stations(
+    id          INT UNSIGNED        NOT NULL,
+    name        VARCHAR(64),
+    payTerminal BOOLEAN,
+    capacity    SMALLINT UNSIGNED,
+    gpsx        DECIMAL(9,6),
+    gpsy        DECIMAL(9,6),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Bikes(
+    id              MEDIUMINT UNSIGNED      NOT NULL,
+    commissionDate  DATE,
+    model           VARCHAR(64),
+    state           TEXT,
+    station         INT UNSIGNED,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (station) REFERENCES Stations(id)
+);
+
+CREATE TABLE Users(
+    id              INT UNSIGNED        NOT NULL,
+    password        TINYTEXT,
+    expiryDate      DATETIME,
+    cardNumber      BIGINT(16),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Subscribers(
+    id                  INT UNSIGNED        NOT NULL,
+    rfid                INT UNSIGNED,
+    lastName            TINYTEXT,
+    firstName           TINYTEXT,
+    addressTown         TINYTEXT,
+    addressZIP          MEDIUMINT,
+    addressStreet       TINYTEXT,
+    addressNumber       VARCHAR(10),
+    registrationDate    DATETIME,
+    phone               VARCHAR(20),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES Users(id)
+);
+
+CREATE TABLE Trips(
+    user            INT UNSIGNED      NOT NULL,
+    startDate       TEXT,
+    endDate         TEXT,
+    startStation    INT UNSIGNED,
+    endStation      INT UNSIGNED,
+    bike            INT UNSIGNED,
+    paid            BOOLEAN,
+
+    PRIMARY KEY (user, startDate),
+    FOREIGN KEY (user) REFERENCES Users(id),
+    FOREIGN KEY (startStation) REFERENCES Stations(id),
+    FOREIGN KEY (endStation) REFERENCES Stations(id),
+    FOREIGN KEY (bike) REFERENCES Bikes(id)
+);
