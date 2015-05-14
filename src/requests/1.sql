8c56fce51c3005c7f92a601e7464d21a83a7479a
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-05-14 20:59:38 +0200

    [enh] compressed request 1 a bit

--- src/requests/1.sql
+++ src/requests/1.sql
@@ -1,6 +1,5 @@
 SELECT DISTINCT Subscribers.id, Subscribers.firstName, Subscribers.lastName
 FROM Trips
 INNER JOIN Subscribers ON Subscribers.id = Trips.user AND Subscribers.addressZIP = "1050"
-INNER JOIN Stations ON Stations.id = Trips.startStation
-WHERE Stations.name = "FLAGEY"
+INNER JOIN Stations ON Stations.id = Trips.startStation AND Stations.name = "FLAGEY"
 ORDER BY Subscribers.id;
